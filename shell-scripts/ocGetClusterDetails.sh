0d60a7f93db764ec141a6b53c185a98db986b49c
asaket <ashish.saket@gmail.com>
2024-08-05 16:24:56 +0800

    Updated ocGetClusterDetails.sh to fix echo and tail command options for bash shell

--- shell-scripts/ocGetClusterDetails.sh
+++ shell-scripts/ocGetClusterDetails.sh
@@ -2,24 +2,24 @@
 
 oc get clusterversion
 
-echo "\n[Cluster Nodes] =====>"
+echo -e "\n[Cluster Nodes] =====>"
 # get node instance-type, cpu, memory
 oc get nodes -o json | jq -r '.items[] | [.metadata.name, .metadata.labels["node.kubernetes.io/instance-type"], .status.capacity["cpu"], .status.capacity["memory"]] | @tsv' > temp.file.oc.1
 
 # get node roles
-oc get nodes | awk '{ print $3 }' | tail +2 > temp.file.oc.2
+oc get nodes | awk '{ print $3 }' | tail -n +2 > temp.file.oc.2
 
 # get node taints
-for node in `oc get nodes | awk '{ print $1 }' | tail +2`; do oc describe node $node | grep -i taint | awk '{ print $2 }'; done > temp.file.oc.3
+for node in `oc get nodes | awk '{ print $1 }' | tail -n +2`; do oc describe node $node | grep -i taint | awk '{ print $2 }'; done > temp.file.oc.3
 
 paste temp.file.oc.1 temp.file.oc.2 temp.file.oc.3 | sort -k 5
 
 rm temp.file.oc.*
 
 # get cluster network
-echo "\n[Cluster Network] =====>"
+echo -e "\n[Cluster Network] =====>"
 oc get network/cluster -o yaml
 
 # get resource quotas
-echo "\n[Resource Quotas] =====>"
+echo -e "\n[Resource Quotas] =====>"
 oc get resourcequotas -A -o json | jq -r '(["Namespace","CPU Limit (spec.hard)","CPU Limit (status.used)"] | @csv), (.items[] | [.metadata.namespace, .spec.hard["limits.cpu"], .status.used["limits.cpu"]] | @csv)'
